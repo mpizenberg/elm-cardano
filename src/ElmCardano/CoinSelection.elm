bc8b5f6f8dc69bb99b54842dac1501f40fd414bf
Clark Alesna <clark_alesna@hotmail.com>
2023-10-30 15:53:41 +0800

    Implement Largest-First Coin Selection Algorithm (CIP2) (#5)
    * Basic Largest First Coin Selection
    
    * simplier expected result
    
    * fixed the link
    
    * - Added more simple tests
    - Addressed some PR requested change
    
    * Adjusted to fit new structure
    
    * valueToLovelace not needed anymore?
    
    * moved lovelace function to Utxo module
    
    * Added documentation
    
    * availableUtxo to availableUtxos for consistency
    
    * Basic Largest First Coin Selection
    
    * simplier expected result
    
    * fixed the link
    
    * - Added more simple tests
    - Addressed some PR requested change
    
    * Adjusted to fit new structure
    
    * valueToLovelace not needed anymore?
    
    * moved lovelace function to Utxo module
    
    * Added documentation
    
    * availableUtxo to availableUtxos for consistency
    
    * Rework API of 'CoinSelection' module
    
      - [x] Avoid repeating 'CoinSelection' in names -> module already serve as
        namespaces.
    
      - [x] Move more functions to `ElmCardano.Utxo` as they do not relate
        to coin selection but are more generic.
    
      - [x] Change `requestedOutputs` to `targetAmount` as we select towards
        a target amount and not towards a set of outputs; the addresses and
        datums of the `requestedOutputs` are of no-use. Plus, this makes it
        clearer that this coin selection only cares about Ada/Lovelace.
    
      - [x] Remove the `requestedOutputs` from the result; it's redundant as
        already given as argument.
    
      - [x] Adjust tests accordingly and use smart constructors in tests to
        also reduce noise.
    
    * Add properties to coin selection / largest-first tests.
    
    * Fix coin-selection bug caught by property tests.
    
      We gotta count the already selected outputs.
    
    * This commit optimizes the doLargestFirst function within the largestFirst coin selection algorithm. Previously, the function was using List.length to check the number of selected UTXOs on each recursive call, which has a time complexity of O(n).
    
    * Reorder nmax and context arguments
    
    In elm, we tend to prefer the most data-relevant argument
    to be last, as it can be used in a pipeline.
    
    buildSomeContext
      |> largestFirst nmax
    
    * Split the function doc title
    
    * Rename nmax to maxInputCount
    
    * Use clearer names in the recursive function
    
    * Use a record for arguments
    
    * Format
    
    ---------
    
    Co-authored-by: KtorZ <matthias.benkort@gmail.com>
    Co-authored-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

---
+++ src/ElmCardano/CoinSelection.elm
@@ -0,0 +1,126 @@
+module ElmCardano.CoinSelection exposing
+    ( Context, Error(..), Selection
+    , largestFirst
+    )
+
+{-| Module `ElmCardano.CoinSelection` provides functionality for performing
+coin selection based on a set of available UTXOs and a set of requested outputs.
+It exports functions for sorting UTXOs and performing the Largest-First coin
+selection algorithm as described in CIP2 (<https://cips.cardano.org/cips/cip2/>).
+
+
+# Types
+
+@docs Context, Error, Selection
+
+
+# Strategies
+
+@docs largestFirst
+
+-}
+
+import Bytes.Comparable exposing (Bytes)
+import ElmCardano.Utxo
+    exposing
+        ( Output
+        , fromLovelace
+        , lovelace
+        , sortByDescendingLovelace
+        , totalLovelace
+        )
+
+
+{-| Enumerates the possible errors that can occur during coin selection.
+-}
+type Error
+    = MaximumInputCountExceeded
+    | UTxOBalanceInsufficient
+
+
+{-| Represents the result of a successful coin selection.
+-}
+type alias Selection =
+    { selectedOutputs : List Output
+    , changeOutput : Maybe Output
+    }
+
+
+{-| Holds the arguments necessary for performing coin selection.
+-}
+type alias Context =
+    { availableOutputs : List Output
+    , alreadySelectedOutputs : List Output
+    , targetAmount : Int
+    , changeAddress : Bytes
+    }
+
+
+{-| Implements the Largest-First coin selection algorithm as described in CIP2.
+
+Takes a `Context` record containing the available UTXOs, initially
+selected UTXOs, requested outputs, and change address, along with an `Int`
+representing the maximum number of inputs allowed. Returns either a
+`Error` or a `Selection`. See <https://cips.cardano.org/cips/cip2/#largestfirst>
+
+-}
+largestFirst : Int -> Context -> Result Error Selection
+largestFirst maxInputCount context =
+    let
+        sortedAvailableUtxo =
+            sortByDescendingLovelace context.availableOutputs
+
+        remainingAmount =
+            context.targetAmount - totalLovelace context.alreadySelectedOutputs
+    in
+    doLargestFirst
+        { maxInputCount = maxInputCount
+        , selectedInputCount = List.length context.alreadySelectedOutputs
+        , remainingAmount = remainingAmount
+        , availableOutputs = sortedAvailableUtxo
+        , selectedOutputs = context.alreadySelectedOutputs
+        }
+        |> Result.map (\withAddress -> withAddress context.changeAddress)
+
+
+
+-- doLargestFirst : Int -> Int -> Int -> List Output -> List Output -> Result Error (Bytes -> Selection)
+
+
+doLargestFirst :
+    { maxInputCount : Int
+    , selectedInputCount : Int
+    , remainingAmount : Int
+    , availableOutputs : List Output
+    , selectedOutputs : List Output
+    }
+    -> Result Error (Bytes -> Selection)
+doLargestFirst { maxInputCount, selectedInputCount, remainingAmount, availableOutputs, selectedOutputs } =
+    if selectedInputCount > maxInputCount then
+        Err MaximumInputCountExceeded
+
+    else if remainingAmount > 0 then
+        case availableOutputs of
+            [] ->
+                Err UTxOBalanceInsufficient
+
+            utxo :: utxos ->
+                doLargestFirst
+                    { maxInputCount = maxInputCount
+                    , selectedInputCount = selectedInputCount + 1
+                    , remainingAmount = remainingAmount - lovelace utxo
+                    , availableOutputs = utxos
+                    , selectedOutputs = utxo :: selectedOutputs
+                    }
+
+    else
+        Ok <|
+            \changeAddress ->
+                { selectedOutputs = selectedOutputs
+                , changeOutput =
+                    if remainingAmount == 0 then
+                        Nothing
+
+                    else
+                        Just (fromLovelace changeAddress -remainingAmount)
+                }
---
+++ tests/ElmCardano/CoinSelectionTests.elm
@@ -0,0 +1,216 @@
+module ElmCardano.CoinSelectionTests exposing (..)
+
+import Bytes.Comparable as Bytes exposing (Bytes)
+import ElmCardano.CoinSelection as CoinSelection exposing (Error(..), largestFirst)
+import ElmCardano.Utxo exposing (fromLovelace, lovelace, totalLovelace)
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer)
+import Result.Extra as Result
+import Test exposing (Test, describe, expectDistribution, fuzzWith, test)
+import Test.Distribution as Distribution
+
+
+suite : Test
+suite =
+    describe "CoinSelection"
+        [ describe "largestFirst"
+            [ test "basic scenario" <| basicScenarioTest
+            , test "no utxos" <| noOutputsTest
+            , test "insufficient funds" <| insufficientFundsTest
+            , test "single utxo, single output, equal value" <| singleUtxoSingleOutputEqualValueTest
+            , fuzzCoinSelection "coverage of payments" propCoverageOfPayment
+            , fuzzCoinSelection "correctness of change" propCorrectnessOfChange
+            ]
+        ]
+
+
+basicScenarioTest : a -> Expectation
+basicScenarioTest _ =
+    let
+        context =
+            { availableOutputs =
+                [ fromLovelace (address "1") 50
+                , fromLovelace (address "2") 30
+                , fromLovelace (address "3") 20
+                ]
+            , alreadySelectedOutputs = []
+            , targetAmount = 30
+            , changeAddress =
+                address "change"
+            }
+
+        maxInputCount =
+            5
+
+        expectedResult =
+            Ok
+                { selectedOutputs = [ fromLovelace (address "1") 50 ]
+                , changeOutput = Just <| fromLovelace context.changeAddress 20
+                }
+    in
+    largestFirst maxInputCount context
+        |> Expect.equal expectedResult
+
+
+noOutputsTest : a -> Expectation
+noOutputsTest _ =
+    let
+        context =
+            { availableOutputs = []
+            , alreadySelectedOutputs = []
+            , targetAmount = 30
+            , changeAddress = address "change"
+            }
+
+        maxInputCount =
+            5
+    in
+    largestFirst maxInputCount context
+        |> Expect.equal (Err UTxOBalanceInsufficient)
+
+
+insufficientFundsTest : a -> Expectation
+insufficientFundsTest _ =
+    let
+        availableOutputs =
+            [ fromLovelace (address "1") 5
+            , fromLovelace (address "2") 10
+            ]
+
+        context =
+            { availableOutputs = availableOutputs
+            , alreadySelectedOutputs = []
+            , targetAmount = 30
+            , changeAddress = address "change"
+            }
+
+        result =
+            largestFirst 5 context
+    in
+    Expect.equal (Err UTxOBalanceInsufficient) result
+
+
+singleUtxoSingleOutputEqualValueTest : a -> Expectation
+singleUtxoSingleOutputEqualValueTest _ =
+    let
+        context =
+            { availableOutputs = [ fromLovelace (address "1") 10 ]
+            , alreadySelectedOutputs = []
+            , targetAmount = 10
+            , changeAddress = address "change"
+            }
+
+        maxInputCount =
+            5
+
+        expectedResult =
+            Ok
+                { selectedOutputs = context.availableOutputs
+                , changeOutput = Nothing
+                }
+    in
+    largestFirst maxInputCount context
+        |> Expect.equal expectedResult
+
+
+
+-- Fixtures
+
+
+address : String -> Bytes
+address suffix =
+    Bytes.fromStringUnchecked <| "addr" ++ suffix
+
+
+
+-- Fuzzer
+
+
+fuzzCoinSelection : String -> (Int -> CoinSelection.Context -> Expectation) -> Test
+fuzzCoinSelection title prop =
+    let
+        maxInputCount =
+            5
+    in
+    fuzzWith
+        { runs = 100
+        , distribution = contextDistribution maxInputCount
+        }
+        (contextFuzzer maxInputCount)
+        title
+        (prop maxInputCount)
+
+
+contextFuzzer : Int -> Fuzzer CoinSelection.Context
+contextFuzzer maxInputCount =
+    let
+        maxInt =
+            100
+
+        outputFuzzer =
+            Fuzz.map2 fromLovelace
+                (Fuzz.int |> Fuzz.map (\i -> address <| "_" ++ String.fromInt i))
+                (Fuzz.intRange 1 maxInt)
+    in
+    Fuzz.map4 CoinSelection.Context
+        (Fuzz.frequency
+            [ ( 1, Fuzz.constant [] )
+            , ( 9, Fuzz.listOfLengthBetween 1 (maxInputCount + 1) outputFuzzer )
+            ]
+        )
+        (Fuzz.frequency
+            [ ( 1, Fuzz.listOfLengthBetween 0 maxInputCount outputFuzzer )
+            , ( 9, Fuzz.constant [] )
+            ]
+        )
+        (Fuzz.intRange 0 maxInt)
+        (Fuzz.constant <| address "change")
+
+
+contextDistribution : Int -> Test.Distribution CoinSelection.Context
+contextDistribution maxInputCount =
+    expectDistribution
+        [ ( Distribution.atLeast 70
+          , "success"
+          , \ctx -> largestFirst maxInputCount ctx |> Result.isOk
+          )
+        , ( Distribution.atLeast 80
+          , "no already selected outputs"
+          , \ctx -> ctx.alreadySelectedOutputs |> List.isEmpty
+          )
+        , ( Distribution.atLeast 5
+          , "already selected outputs"
+          , \ctx -> ctx.alreadySelectedOutputs |> List.isEmpty |> not
+          )
+        ]
+
+
+
+-- Properties
+
+
+propCoverageOfPayment : Int -> CoinSelection.Context -> Expectation
+propCoverageOfPayment maxInputCount context =
+    case largestFirst maxInputCount context of
+        Err _ ->
+            Expect.pass
+
+        Ok { selectedOutputs } ->
+            totalLovelace selectedOutputs |> Expect.atLeast context.targetAmount
+
+
+propCorrectnessOfChange : Int -> CoinSelection.Context -> Expectation
+propCorrectnessOfChange maxInputCount context =
+    case largestFirst maxInputCount context of
+        Err _ ->
+            Expect.pass
+
+        Ok { selectedOutputs, changeOutput } ->
+            let
+                change =
+                    changeOutput
+                        |> Maybe.map lovelace
+                        |> Maybe.withDefault 0
+            in
+            totalLovelace selectedOutputs
+                |> Expect.equal (change + context.targetAmount)
