a702364ece96e37092c27c8e23fab42dacdf09a6
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2024-07-28 15:06:45 +0200

    Make some more progress with Tx building

--- src/Cardano.elm
+++ src/Cardano.elm
@@ -197,7 +197,6 @@ Now we want to retrieve 1 ada from it, and keep the other ada locked.
     unlockTx =
         initTx
             |> spendFromNativeScript lockScriptSource AutoUtxoSelection oneAda
-            |> addRequiredSigners [ toMe.paymentKey ]
             |> transfer [] [ { destination = toMe, assets = oneAda } ]
             -- changeBackToSource will send the 1 ada change back to the contract address
             |> handleChange changeBackToSource
@@ -258,8 +257,8 @@ Ok, let’s start with the minting and burning example.
 
 Ok now let’s show how sending to a Plutus script would be done.
 As before, we’ll use the simple example of a lock script.
-But this time, we don’t write it directly (as in the NativeScript example),
-instead we suppose the contract was written in another language (Aiken, plu-ts, Opshin, ...).
+But this time, we don’t write it directly (as in the NativeScript example).
+Instead we suppose the contract was written in another language (Aiken, plu-ts, Opshin, ...).
 But the blueprint of the contract, with its hash is available.
 
     lockScriptHash =
@@ -285,6 +284,80 @@ But the blueprint of the contract, with its hash is available.
 For such a tiny contract, which just tests if our signature is present,
 no need to put it in a reference UTxO first.
 We can embed it directly in the transaction.
+
+    lockScriptSource =
+        -- script and scriptHash are coming from the blueprint
+        EmbeddedPlutusScript { script = lockScript, scriptHash = lockScriptHash }
+
+    localState =
+        -- We already updated a list of our UTxOs
+        { utxos = myUtxos ++ scriptUtxos }
+
+    -- We manually identify the locked UTxO we want to consume
+    -- in order to retrieve 1 ada from it.
+    lockedUtxoSelection =
+        ManualScriptUtxoSelection
+            [ { ref = theKnownOutputRef
+              , utxo = theKnownUtxo
+              , redeemer = Data.Int Integer.zero -- unused here anyway
+              }
+            ]
+
+    unlockTx =
+        initTx
+            |> spendFromPlutusScript lockScriptSource lockedUtxoSelection oneAda
+            -- required signature for the plutus script to check
+            |> addRequiredSigners [ toMe.paymentKey ]
+            |> transfer [] [ { destination = toMe, assets = oneAda } ]
+            -- need a manual handling of the change this time
+            |> handleChange manualChangeReallocation
+            |> payFeesWithAccount fromMe AutoComputeFees
+            |> finalizeTx Mainnet costModels localState defaultSelectionAlgo
+            |> signTx
+
+    -- Send back 1 ada to the lock script.
+    -- Retrieve the rest back to me.
+    manualChangeReallocation allChange =
+        let
+            accumulatedChange =
+                Value.sum (List.map Tuple.second allChange)
+
+            backToScript =
+                oneAda
+
+            restOfChange =
+                Value.diff accumulatedChange backToScript
+        in
+        { toOwners = [ ( toMe, restOfChange ) ]
+        , toNativeScripts = []
+        , toPlutusScripts =
+            [ { scriptHash = lockScriptHash
+              , stakeCred = myStakeCredential
+              , datum = Data.Bytes (Bytes.toAny toMe.paymentKey)
+              , assets = backToScript
+              }
+            ]
+        }
+
+Ok this was a bit more code than before, but I hope it is still very clear.
+There are three novelties in the above example.
+
+First, we manually selected the utxo we want to consume to unlock our 1 ada.
+Since UTxOs in Plutus scripts have datum attached, they are not super fungible.
+We could use an autoselection, with care,
+but it’s just simpler to manually tell which one to consume.
+
+Second is the manual change reallocation.
+Since we only retrieve 1 ada of the 2 adas, we want to put 1 ada back.
+But we don’t want to use the automatic `changeBackToSource` function
+that we used up to now.
+If coded well, it could default to copy the previous datum,
+but if we are just unsure of its behavior, its simpler to manually control the change.
+
+Third, this time we decided to specify who is paying the fees.
+It is us here, but it doesn’t have to be.
+This way, the finalization step will know where to look to pay for the fees.
+The default behavior tries to just use lovelaces from some of the spent UTxOs.
 
 
 ## Code Documentation
@@ -391,17 +464,27 @@ simpleTransfer source destination assets =
 {-| -}
 type BasicUtxoSelection
     = AutoUtxoSelection
-    | ManualUtxoSelection (List OutputReference)
+      -- TODO: think if the Output is needed or not in manual selection.
+      -- In theory not, as long as its output reference
+      -- is present in the LocalState we use in finalizeTx.
+    | ManualUtxoSelection (List ( OutputReference, Output ))
 
 
 {-| -}
 transfer :
     List { source : SourceOwner, utxoSelection : BasicUtxoSelection, assets : Value }
     -> List { destination : DestinationOwner, assets : Value }
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 transfer sources destinations tx =
     Debug.todo "transfer"
+
+
+{-| Custom construct for specific needs involving a datum or reference script.
+-}
+createOutput : Output -> Tx a -> Tx a
+createOutput output tx =
+    Debug.todo "create output"
 
 
 
@@ -424,8 +507,8 @@ type NativeScriptSource
 mintAndBurnViaNativeScript :
     NativeScriptSource
     -> List { asset : Bytes AssetName, amount : Integer }
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 mintAndBurnViaNativeScript scriptSource amounts tx =
     Debug.todo "native mint / burn"
 
@@ -435,8 +518,8 @@ sendToNativeScript :
     Bytes CredentialHash
     -> Maybe StakeCredential
     -> Value
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 sendToNativeScript scriptHash maybeStakeCredential assets tx =
     Debug.todo "sendToNativeScript"
 
@@ -446,8 +529,8 @@ spendFromNativeScript :
     NativeScriptSource
     -> BasicUtxoSelection
     -> Value
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 spendFromNativeScript scriptHash utxoSelection assets tx =
     Debug.todo "spendFromNativeScript"
 
@@ -473,8 +556,8 @@ mintAndBurnViaPlutusScript :
     PlutusScriptSource
     -> Data
     -> List { asset : Bytes AssetName, amount : Integer }
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 mintAndBurnViaPlutusScript scriptSource redeemer amounts tx =
     Debug.todo "plutus mint / burn"
 
@@ -485,8 +568,8 @@ sendToPlutusScript :
     -> Maybe StakeCredential
     -> Data
     -> Value
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 sendToPlutusScript scriptHash maybeStakeCredential datum assets tx =
     Debug.todo "send to plutus script"
 
@@ -494,11 +577,26 @@ sendToPlutusScript scriptHash maybeStakeCredential datum assets tx =
 {-| -}
 type ScriptUtxoSelection
     = AutoScriptUtxoSelection ({ ref : OutputReference, utxo : Output } -> Maybe { redeemer : Data })
-    | ManualScriptUtxoSelection (List { ref : OutputReference, redeemer : Data })
+    | ManualScriptUtxoSelection
+        (List
+            { ref : OutputReference
+
+            -- TODO: think if the Output is needed or not in manual selection.
+            -- In theory not, as long as its output reference
+            -- is present in the LocalState we use in finalizeTx.
+            , utxo : Output
+            , redeemer : Data
+            }
+        )
 
 
 {-| -}
-spendFromPlutusScript : Bytes CredentialHash -> ScriptUtxoSelection -> Value -> Intent
+spendFromPlutusScript :
+    PlutusScriptSource
+    -> ScriptUtxoSelection
+    -> Value
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : Needs }
 spendFromPlutusScript scriptHash utxoSelection totalSpent =
     Debug.todo "spend from plutus script"
 
@@ -507,39 +605,6 @@ spendFromPlutusScript scriptHash utxoSelection totalSpent =
 withdrawViaPlutusScript : Bytes CredentialHash -> Data -> Natural -> Intent
 withdrawViaPlutusScript scriptHash redeemer adaLovelaces =
     Debug.todo "withdraw via plutus script"
-
-
-
--- Handling change for non-allocated values from spent utxos
-
-
-{-| -}
-type alias ChangeReallocation =
-    { toOwners : List ( DestinationOwner, Value )
-    , toNativeScripts : List { scripthHash : Bytes CredentialHash, stakeCred : Maybe StakeCredential, assets : Value }
-    , toPlutusScripts : List { scripthHash : Bytes CredentialHash, stakeCred : Maybe StakeCredential, datum : Data, assets : Value }
-    }
-
-
-{-| -}
-handleChange :
-    (List ( Output, Value ) -> ChangeReallocation)
-    -> Tx { build | handleChange : Needs }
-    -> Tx { build | handleChange : HasOrNoNeed }
-handleChange reallocateChange =
-    Debug.todo "handle change"
-
-
-{-| -}
-changeBackToSource : List ( Output, Value ) -> ChangeReallocation
-changeBackToSource change =
-    Debug.todo "change back to source"
-
-
-{-| -}
-changeBackTo : DestinationOwner -> List ( Output, Value ) -> ChangeReallocation
-changeBackTo destination change =
-    Debug.todo "change back to"
 
 
 
@@ -569,18 +634,71 @@ addRequiredSigners signers =
 
 
 
+-- Handling change for non-allocated values from spent utxos
+
+
+{-| -}
+type alias ChangeReallocation =
+    { toOwners : List ( DestinationOwner, Value )
+    , toNativeScripts :
+        List
+            { scripthHash : Bytes CredentialHash
+            , stakeCred : Maybe StakeCredential
+            , assets : Value
+            }
+    , toPlutusScripts :
+        List
+            { scripthHash : Bytes CredentialHash
+            , stakeCred : Maybe StakeCredential
+            , datum : Data
+            , assets : Value
+            }
+    }
+
+
+{-| -}
+handleChange :
+    (List ( Output, Value ) -> ChangeReallocation)
+    -> Tx { a | handleChange : Needs }
+    -> Tx { a | handleChange : HasOrNoNeed }
+handleChange reallocateChange =
+    Debug.todo "handle change"
+
+
+{-| -}
+changeBackToSource : List ( Output, Value ) -> ChangeReallocation
+changeBackToSource change =
+    Debug.todo "change back to source"
+
+
+{-| -}
+changeBackTo : DestinationOwner -> List ( Output, Value ) -> ChangeReallocation
+changeBackTo destination change =
+    Debug.todo "change back to"
+
+
+
 -- Requirements
 
 
 {-| -}
-setFeesManually : Natural -> Intent
-setFeesManually adaLovelaces =
+type FeeStrat
+    = AutoComputeFees
+    | ManualFeeAmount Natural
+
+
+{-| -}
+payFeesWithAccount :
+    SourceOwner
+    -> FeeStrat
+    -> Tx { a | handleChange : HasOrNoNeed }
+    -> Tx { a | handleChange : HasOrNoNeed }
+payFeesWithAccount source strat tx =
     Debug.todo "manual fees"
 
 
 
 -- Finalizing the Tx
--- = AutoScriptUtxoSelection ({ ref : OutputReference, utxo : Output } -> Maybe { redeemer : Data })
 
 
 {-| -}
@@ -603,7 +721,7 @@ finalizeTx :
     -> CostModels
     -> LocalState
     -> CoinSelection.Algorithm
-    -> Tx { build | handleChange : HasOrNoNeed }
-    -> Result String (Tx { build | final : () })
+    -> Tx { a | handleChange : HasOrNoNeed }
+    -> Result String (Tx { a | final : () })
 finalizeTx networkId costModels localState selectionAlgo tx =
     Debug.todo "finalize tx"
