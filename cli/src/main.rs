2fee9cd18c1aa562e15c829300cb44b7d0e4b21a
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2024-09-20 08:37:11 +0200

    make wasm loading for both node/web in the patch

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -149,15 +149,27 @@ fn run_subcommand(run_args: RunSubCommand) -> anyhow::Result<()> {
 /// Kernel patching the elm compiler output with uplc_wasm
 fn kernel_patching_uplc_wasm_iife(elm_js: &str) -> String {
     let eval_def = r#"
+let uplc_wasm_module;
 let evalScriptsCostsKernel = (elm_args) => {
   try {
-    if (!("uplc_wasm" in window)) {
-      throw new Error("Missing uplc_wasm module in window. Make sure to dynamically import the Elm app after window.uplc_wasm was populated.");
+    if (!uplc_wasm_module) {
+      if (typeof window !== 'undefined' && window.document) {
+        // Browser environment
+        if (!("uplc_wasm" in window)) {
+          throw new Error("Missing uplc_wasm module in window. Make sure to dynamically import the Elm app after window.uplc_wasm was populated.");
+        }
+        uplc_wasm_module = window.uplc_wasm;
+      } else if (typeof process !== 'undefined' && process.versions && process.versions.node) {
+        // Node.js environment
+        uplc_wasm_module = require("./pkg-node/uplc_wasm.js");
+      } else {
+        throw new Error("Unknown environment");
+      }
     }
     const args = elm_args.a; // elm uses a specific structure for its data
     const fromHexString = (hexString) =>
       Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
-    let redeemers = window.uplc_wasm.eval_phase_two_raw(
+    let redeemers = uplc_wasm_module.eval_phase_two_raw(
       fromHexString(args.tx_bytes), // tx_bytes: &[u8],
       args.utxos_refs_bytes.map(fromHexString), // utxos_refs_bytes: Vec<js_sys::Uint8Array>,
       args.utxos_outputs_bytes.map(fromHexString), // utxos_outputs_bytes: Vec<js_sys::Uint8Array>,
