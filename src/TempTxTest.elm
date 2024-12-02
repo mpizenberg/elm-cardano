ce8550551257a44bb1f410d728ad67d9ad336e98
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2024-12-02 10:19:04 +0100

    Remove and ignore temp file

--- src/TempTxTest.elm
+++
@@ -1,8 +0,0 @@
-module TempTxTest exposing (toto)
-
--- Temporary module, used to format Tx stuff before giving it to Claude.
--- See claude-sonnet-transform-elm.txt for the actual Claude stuff.
-
-
-toto =
-    Nothing
